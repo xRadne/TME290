54bc560fc0a66ad3fea9a5d40c806646be2d66b3
xRadne <alex.radne@gmail.com>
2020-03-30 20:53:36 +0200

    Using OD4

--- mytest/helloworld.cpp
+++ mytest/helloworld.cpp
@@ -9,35 +9,26 @@ int32_t main(int32_t, char **) {
 	PrimeChecker pc;
 	std::cout << "Hello world = " << pc.isPrime(23) << std::endl;
 
-	cluon::UDPSender sender{"225.0.0.111", 1238};
+  cluon::OD4Session od4(111, 
+    [](cluon::data::Envelope &&envelope) noexcept {
+      if (envelope.dataType() == 2001) {
+        MyTestMessage1 receivedMsg = 
+          cluon::extractMessage<MyTestMessage1>(std::move(envelope));
+        PrimeChecker pc;
+        std::cout << receivedMsg.myValue() << " is " 
+          << (pc.isPrime(receivedMsg.myValue()) ? "" : "not") << " a prime" 
+          << std::endl;
+      } 
+    });
 	
 	uint16_t value;
 	std::cout << "Enter a number to check: ";
 	std::cin >> value;
+	
 	MyTestMessage1 msg;
 	msg.myValue(value);
-	cluon::ToProtoVisitor encoder;
-	msg.accept(encoder);
-	std::string data{encoder.encodedData()};
-	sender.send(std::move(data));
-
-	std::this_thread::sleep_for(std::chrono::duration<double>(5.0));
-
-	cluon::UDPReceiver receiver("225.0.0.111", 1238,
-		[](std::string &&data, std::string &&/*from*/,
-			std::chrono::system_clock::time_point &&/*timepoint*/) noexcept {
-				std::stringstream sstr{data};
-				cluon::FromProtoVisitor decoder;
-				decoder.decodeFrom(sstr);
-				MyTestMessage1 receivedMsg;
-				receivedMsg.accept(decoder);
-				PrimeChecker pc;
-				std::cout << receivedMsg.myValue() << " is " << (pc.isPrime(receivedMsg.myValue()) ? "" : "not") << " a prime" << std::endl;
-		});
-
-	while (receiver.isRunning()) {
-		std::this_thread::sleep_for(std::chrono::duration<double>(1.0));
-	}
-
+	
+	od4.send(msg);
+	
 	return 0;
 }
